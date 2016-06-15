55c475c9a2f2cacf66df8bd1003e92761af05344
Miha Blazin <miha.blazin@gmail.com>
2016-06-15 23:30:24 +0200

    Added saving for image to home folder

--- index.js
+++ index.js
@@ -3,11 +3,32 @@
 'use strict';
 
 const request = require('request');
+const parseString = require('xml2js').parseString;
+const fs = require('fs');
+
+function downloadImage(url) {
+    const fileName = process.env.HOME + '/' + 'bing_wallpaper.jpg'
+    console.log('Downloading: ' + url + " to " + fileName);
+    request(url).on('error', (err) => {
+        console.log(err);
+    }).pipe(fs.createWriteStream(fileName));
+    console.log('Successfully downloaded image!')
+}
+
+function parseXML(rawString) {
+    parseString(rawString, (err, result) => {
+        if (!err) {
+            downloadImage(result.rss.channel[0].item[0].enclosure[0].$.url);
+        } else {
+            console.log(err)
+        }
+    });
+}
 
 function start() {
     request('http://feeds.feedburner.com/bingimages', (error, response, body) => {
         if (!error && response.statusCode === 200) {
-            console.log(body)
+            parseXML(body);
         } else {
             console.log(error);
         }
@@ -15,4 +36,4 @@ function start() {
 }
 
 // Wait for wifi to connect
-setTimeout(start, 1);
+setTimeout(start, 30000);
